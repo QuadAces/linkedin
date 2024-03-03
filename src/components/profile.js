b46811876352510d61225f8e7b90dffb3a22b1fd
oliver po <oliver.po2882@gmail.com>
2024-03-03 15:51:50 +1100

    added stuff

--- src/components/profile.js
+++ src/components/profile.js
@@ -2,22 +2,6 @@ import Image from "next/image";
 import { MdMessage, MdPhone } from "react-icons/md";
 import Resume from "./resume";
 
-<<<<<<< HEAD
-export default function profile({ name = "John Doe", imgsrc = "/assets/test.png", about = "Sample Text", email = "1234", phone = "1234" }) {
-    return (<div className="flex flex-row mx-16 space-x-8 flex-grow">
-        <div className="flex flex-col space-y-8"><Image
-            src={imgsrc}
-            className=" rounded !h-64 !w-64"
-            width={250} //used to satisfy param requirements but not actually used
-            height={250}
-        /></div>
-
-        <div className="flex flex-col flex-grow space-y-4">
-            <div className="text-6xl pb-4 border-b-1">{name}</div>
-            <div className="flex flex-col space-y-4">
-                <div className="text-lg mt-8 text-wrap">{about}</div>
-            </div>
-=======
 export default function profile({
   name = "John Doe",
   imgsrc = "/assets/test.png",
@@ -35,14 +19,14 @@ export default function profile({
           height={250}
         />
       </div>
->>>>>>> refs/remotes/origin/main
 
       <div className="flex flex-col flex-grow space-y-4">
         <div className="text-6xl pb-4 border-b-1">{name}</div>
         <div className="flex flex-col space-y-4">
-          <div classname="text-lg mt-8 text-wrap">{about}</div>
+          <div className="text-lg mt-8 text-wrap">{about}</div>
         </div>
-        <Resume/>
+
+        <Resume />
       </div>
     </div>
   );
